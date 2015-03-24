9dcf65cf3ed361e18db63655317d22986ee219e1
AviFoxi <frosenox@gmail.com>
2015-03-23 22:30:31 -0400

    and we got some validations

--- app/models/constraint_week_template.rb
+++ app/models/constraint_week_template.rb
@@ -10,13 +10,14 @@ class ConstraintWeekTemplate < ActiveRecord::Base
   validates_presence_of :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday, :max_lookahead_for_reh, :max_lookahead_for_perf
 
   validates :max_lookahead_for_perf, :max_lookahead_for_reh, :numericality => { :greater_than_or_equal_to => 1 }
-  # validate :active_today_or_future, 
+  
+  validate :active_today_or_future 
 
   private
 
   def active_today_or_future
-  	unless (self.active_starting +1).today? || self.active_starting.future? 
-			errors.add(:special_hold_date, 'must not take effect on a past date')
+  	unless (self.active_starting + 1.day).today? || self.active_starting.future? 
+			errors.add(:active_starting, 'must not take effect on a past date')
 		end
 	end
 
