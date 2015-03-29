a94714b3f9f41b736541bae508e0c91aaba6a524
AviFoxi <frosenox@gmail.com>
2015-03-29 18:17:59 -0400

    add create method in cwt controller, add strogn params to cont, moving forward

--- app/controllers/constraint_week_templates_controller.rb
+++ app/controllers/constraint_week_templates_controller.rb
@@ -9,4 +9,43 @@ class ConstraintWeekTemplatesController < ApplicationController
 		@cds = ConstraintDay.all
 		@cwts = ConstraintWeekTemplate.all
 	end
+
+	def create
+		p '#'*80
+		p 'poorams'
+		p "#{params.inspect}"
+		# p '#'*80
+		# p 'cwt_params'
+		# p "#{cwt_params.inspect}"
+
+		
+
+		# @cwt = ConstraintWeekTemplate.new(cwt_params)
+
+		p '#'*80
+		p "cds_from_nums "
+		p "#{cds_from_nums.inspect}"
+
+
+	end
+
+	private 
+
+	def full_params
+		params.require(:constraint_week_template).permit(
+			:active_starting, :name, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday, :max_lookahead_for_reh, :max_lookahead_for_perf, :release_unbooked_perf_lookahead
+    )
+	end
+
+	def cds_from_nums
+		{
+			monday: ConstraintDay.find( full_params['monday']), 
+			tuesday: ConstraintDay.find( full_params['tuesday']), 
+			wednesday: ConstraintDay.find( full_params['wednesday']), 
+			thursday: ConstraintDay.find( full_params['thursday']), 
+			friday: ConstraintDay.find( full_params['friday']), 
+			saturday: ConstraintDay.find( full_params['saturday']), 
+			sunday: ConstraintDay.find( full_params['sunday'])
+		}
+	end
 end
